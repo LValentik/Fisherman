f300ce908d215a7c51d7516d8879266a39b9d325
Lukáš Valentík <lukas.valentik@seznam.cz>
2024-05-05 21:51:37 +0200

    Refactor fish catching logic and inventory management

--- game.py
+++ game.py
@@ -132,6 +132,63 @@ class DataWork:
         finally:
             cur.close()
             conn.close()
+    
+    def StoreShow():
+        try:
+            cur.execute('SELECT * FROM store.items;')
+            store = cur.fetchall()
+            print("Store:")
+            for item in store:
+                print(f"{item[1]}: {item[2]} coins")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
+        finally:
+            cur.close()
+            conn.close()
+            return True
+    def StoreBuy(item_to_buy, quantity_to_buy):
+        try:
+            cur.execute('SELECT * FROM store.items WHERE "item_name" = %s;', (item_to_buy,))
+            item = cur.fetchone()
+
+            if item is None:
+                print(f"The item {item_to_buy} is not available in the store.")
+                return False
+
+            cur.execute('SELECT * FROM accounts.acc WHERE "user" = %s;', (username,))
+            user = cur.fetchone()
+
+            if user[3] < item[2] * quantity_to_buy:
+                print(f"You don't have enough coins to buy {quantity_to_buy} {item_to_buy}. You need {item[2] * quantity_to_buy} coins.")
+                return False
+
+            if quantity_to_buy > item[3]:
+                print(f"There are only {item[3]} {item_to_buy} available in the store.")
+                return False
+
+            cur.execute('UPDATE accounts.acc SET "value" = "value" - %s WHERE "user" = %s;', (item[2] * quantity_to_buy, username))
+            cur.execute('INSERT INTO inventory.inv ("acc", "item", "value", "quantity") VALUES (%s, %s, %s, %s) ON CONFLICT ("acc", "item") DO UPDATE SET "quantity" = inventory.inv."quantity" + %s;', (username, item_to_buy, item[2], quantity_to_buy, quantity_to_buy))
+            conn.commit()
+
+            print(f"Bought {quantity_to_buy} {item_to_buy} for {item[2] * quantity_to_buy} coins.")
+
+            # Remove the item from the store if the user bought all the items
+            if quantity_to_buy >= item[3]:
+                cur.execute('DELETE FROM store.items WHERE "item_name" = %s;', (item_to_buy,))
+                conn.commit()
+
+            return True
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
+
+        finally:
+            cur.close()
+            conn.close()
+            
+            
             
 
 class fishCatch:
@@ -162,80 +219,112 @@ class fishCatch:
 signin = DataWork.LogRes()
 
 while signin:
-   
-    time.sleep(2)
-    fishChoose = random.randint(0, 790)
-    if fishChoose < 400:
-        fishCaught = fishCatch.catchFish(3, 5)
-        if fishCaught:
-            print('Chytl jsi karase!')
-            fishcaughtNum =+ 1
-            fishcaughtValue =+ fishValue['karas']
-            fishcaughtNames.append('karas')
-        else:
-            print('Nechytil jsi rybu')
-    
-    elif fishChoose >= 400 and fishChoose < 600:
-        fishCaught = fishCatch.catchFish(2, 5)
-        if fishCaught:
-            print('Chytl jsi kapra!')
-            fishcaughtNum =+ 1
-            fishcaughtValue =+ fishValue['kapr']
-            fishcaughtNames.append('kapr')
-        else:
-            print('Nechytil jsi rybu')
-    
-    elif fishChoose >=600 and fishChoose < 700:
-        fishCaught = fishCatch.catchFish(2, 10)
-        if fishCaught:
-            print('Chytl jsi pstruha!')
-            fishcaughtNum =+ 1
-            fishcaughtValue =+ fishValue['pstruh']
-            fishcaughtNames.append('pstruh')
-        else:
-            print('Nechytil jsi rybu')
-            
-    elif fishChoose >= 700 and fishChoose < 750:
-        fishCaught = fishCatch.catchFish(1, 10)
-        if fishCaught:
-            print('Chytl jsi štiku!')
-            fishcaughtNum =+ 1
-            fishcaughtValue =+ fishValue['štika']
-            fishcaughtNames.append('štika')
-        else:
-            print('Nechytil jsi rybu')
-            
-    elif fishChoose >= 750 and fishChoose < 775:
-        fishCaught = fishCatch.catchFish(1, 15)
-        if fishCaught:
-            print('Chytl jsi sumce!')
-            fishcaughtNum =+ 1
-            fishcaughtValue =+ fishValue['sumec']
-            fishcaughtNames.append('sumec')
-        else:
-            print('Nechytil jsi rybu')
-    
-    elif fishChoose >= 775 and fishChoose < 785:
-        fishCaught = fishCatch.catchFish(0.8, 15)
-        if fishCaught:
-            print('Chytl jsi žraloka!')
-            fishcaughtNum =+ 1
-            fishcaughtValue =+ fishValue['žralok']
-            fishcaughtNames.append('žralok')
-        else:
-            print('Nechytil jsi rybu')
-            
-    elif fishChoose >= 785 and fishChoose < 790:
-        fishCaught = fishCatch.catchFish(0.6, 10)
-        if fishCaught:
-            print('Chytl jsi velrybu!')
-            fishcaughtNum =+ 1
-            fishcaughtValue =+ fishValue['velryba']
-            fishcaughtNames.append('velryba')
-        else:
-            print('Nechytil jsi rybu')
+    print("Welcome to the game!")
+    print("Press '1' to start fishing")
+    print("Press '2' to show your inventory")
+    print("Press '3' to sell fish")
+    print("Press '4' to show the store")
+    print("Press '5' to buy from the store")
+    print("Press '6' to exit the game")
+    with keyboard.Events() as events:
+        event = events.get()
+        if event is not None:
+            key = event.key
+            if key == keyboard.Key.one:
+                gameStart = True
+            elif key == keyboard.Key.two:
+                DataWork.InvShow()
+            elif key == keyboard.Key.three:
+                fish_to_sell = input("Enter the name of the fish you want to sell: ")
+                quantity_to_sell = int(input("Enter the quantity you want to sell: "))
+                DataWork.InvSell(fish_to_sell, quantity_to_sell)
+            elif key == keyboard.Key.four:
+                DataWork.StoreShow()
+            elif key == keyboard.Key.five:
+                item_to_buy = input("Enter the name of the item you want to buy: ")
+                quantity_to_buy = int(input("Enter the quantity you want to buy: "))
+                DataWork.StoreBuy(item_to_buy, quantity_to_buy)
+            elif key == keyboard.Key.six:
+                DataWork.GameClose()
+                break
+            else:
+                print("Invalid key")
     
     
+    while gameStart:
+        time.sleep(random.uniform(1, 3))  # Randomized time between fish biting
+        print("You've got a bite! Press the key to catch the fish!")
+        time.sleep(1)
+        fishChoose = random.randint(0, 790)
+        if fishChoose < 400:
+            fishCaught = fishCatch.catchFish(3, 5)
+            if fishCaught:
+                print('Chytl jsi karase!')
+                fishcaughtNum += 1
+                fishcaughtValue += fishValue['karas']
+                fishcaughtNames.append('karas')
+            else:
+                print('Nechytil jsi rybu')
+
+        elif fishChoose >= 400 and fishChoose < 600:
+            fishCaught = fishCatch.catchFish(2, 5)
+            if fishCaught:
+                print('Chytl jsi kapra!')
+                fishcaughtNum += 1
+                fishcaughtValue += fishValue['kapr']
+                fishcaughtNames.append('kapr')
+            else:
+                print('Nechytil jsi rybu')
+
+        elif fishChoose >= 600 and fishChoose < 700:
+            fishCaught = fishCatch.catchFish(2, 10)
+            if fishCaught:
+                print('Chytl jsi pstruha!')
+                fishcaughtNum += 1
+                fishcaughtValue += fishValue['pstruh']
+                fishcaughtNames.append('pstruh')
+            else:
+                print('Nechytil jsi rybu')
+
+        elif fishChoose >= 700 and fishChoose < 750:
+            fishCaught = fishCatch.catchFish(1, 10)
+            if fishCaught:
+                print('Chytl jsi štiku!')
+                fishcaughtNum += 1
+                fishcaughtValue += fishValue['štika']
+                fishcaughtNames.append('štika')
+            else:
+                print('Nechytil jsi rybu')
+
+        elif fishChoose >= 750 and fishChoose < 775:
+            fishCaught = fishCatch.catchFish(1, 15)
+            if fishCaught:
+                print('Chytl jsi sumce!')
+                fishcaughtNum += 1
+                fishcaughtValue += fishValue['sumec']
+                fishcaughtNames.append('sumec')
+            else:
+                print('Nechytil jsi rybu')
+
+        elif fishChoose >= 775 and fishChoose < 785:
+            fishCaught = fishCatch.catchFish(0.8, 15)
+            if fishCaught:
+                print('Chytl jsi žraloka!')
+                fishcaughtNum += 1
+                fishcaughtValue += fishValue['žralok']
+                fishcaughtNames.append('žralok')
+            else:
+                print('Nechytil jsi rybu')
+
+        elif fishChoose >= 785 and fishChoose < 790:
+            fishCaught = fishCatch.catchFish(0.6, 10)
+            if fishCaught:
+                print('Chytl jsi velrybu!')
+                fishcaughtNum += 1
+                fishcaughtValue += fishValue['velryba']
+                fishcaughtNames.append('velryba')
+            else:
+                print('Nechytil jsi rybu')
     
     print("\033[H\033[J", end="")
 
